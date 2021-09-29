3e553ccc429a4302f65762ca91a39f1effd16cd9
hartnetl <laura.hartnett@outlook.com>
2021-09-29 15:41:50 +0000

    Complete lesson 5.2 - models part 2

--- todo/models.py
+++ todo/models.py
@@ -8,3 +8,6 @@ class Item(models.Model):
     # charfield means it will be text. null=False and blank=False makes it a required field
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
