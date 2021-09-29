feb034557c74ad4b25f0f23aa17f1b8abaae5378
hartnetl <laura.hartnett@outlook.com>
2021-09-29 16:04:13 +0000

    Complete lesson 5.1 - rendering data pt1

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Item
 # Create your views here.
 
 
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    # Let's get a query set of all items in the database
+    items = Item.objects.all()
+    # context will be a dictionary with all of our items
+    context = {
+        'items': items
+    }
+    return render(request, 'todo/todo_list.html', context)
