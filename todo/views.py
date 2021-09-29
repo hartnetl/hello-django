cd46e43ce07ced015d08d2c1851a0ddbfbcfb16a
hartnetl <laura.hartnett@outlook.com>
2021-09-29 17:09:03 +0000

    Finish lesson 6.4 - creating new items for user pt2

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 # Create your views here.
 
@@ -12,5 +12,17 @@ def get_todo_list(request):
     }
     return render(request, 'todo/todo_list.html', context)
 
+
 def add_item(request):
+    # Our post button is to here, so we need to handle that properly
+    if request.method == 'POST':
+        # this get the value entered into the input field
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+
+        # Create the new object as per the input fields
+        Item.objects.create(name=name, done=done)
+
+        # bring the user back to the updated todo list
+        return redirect('get_todo_list')
     return render(request, 'todo/add_item.html')
