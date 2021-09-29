8a521318687da81b9354e35b38da09afc31e3f77
hartnetl <laura.hartnett@outlook.com>
2021-09-29 15:26:33 +0000

    Complete lesson 5.1 - models part 1

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# we want to inherit the base model class
+class Item(models.Model):
+    # define the attributes that our individual items will have.
+    # charfield means it will be text. null=False and blank=False makes it a required field
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
