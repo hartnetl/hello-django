a1135b2bbd2a476a64885fcce5ef41d4d8ca4d99
hartnetl <laura.hartnett@outlook.com>
2021-09-30 15:39:33 +0000

    Complete lesson 8.4 - testing models

--- todo/test_models.py
+++ todo/test_models.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Item
 
 # Create your tests here.
+
+class TestModels(TestCase):
+
+    def test_done_default_false(self):
+        # create an instance of an item and check its false
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
